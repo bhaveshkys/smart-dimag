6fffa72824b1fb685092b84c7ee2e0041212d4d1
bhaveshkys <bhaveshsharma8@gmail.com>
2021-07-09 15:53:13 +0530

    trying media queries(2)

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -4,8 +4,8 @@ function ImageLinkForm({onInputChange,onButtonClick}) {
     return (
         <div className=' tc'>    
             <h3>This smart brain will detect faces in any picture.Give it a try</h3>
-            <div className='ml6 mr6 shadow-3 pa3 tc pattern ' >
-                <input type={Text} className="w-70-ns w-30" onChange={onInputChange}></input>
+            <div className='ml6-ns mr6-ns shadow-3 pa3 tc pattern ' >
+                <input type={Text} className="w-70-ns " onChange={onInputChange}></input>
                 <button onClick={onButtonClick} className=" w-30-m grow f4-ns f6 ph3 pv2 dib white bg-light-purple ">detect</button>
             </div>        
         </div>
