5dc66a7f64e257ae746e12e491ab5d074ba39407
bhaveshkys <bhaveshsharma8@gmail.com>
2021-07-10 21:51:51 +0530

    deploying multiface support(1)

--- src/Components/FaceRecog/FaceRecog.js
+++ src/Components/FaceRecog/FaceRecog.js
@@ -2,16 +2,13 @@ import React from 'react';
 import './FaceRecog.css';
 import 'tachyons';
 const FaceRecog=( { imageurl, boxes } )=> {
+    const faces = boxes.map((box,i)=>{  return(<div key={i} className="bounding-box" style={{top:box.topRow, right:box.rightCol,bottom:box.bottomRow,left:box.leftCol}}></div>)})
     return (
         <div className='mw6 center '>
             <div className='  absolute mt2' >
                 <img id="inputimage" className='mt2'   src={imageurl} alt='' width='500px' height='auto' />
                 
-                    {boxes.map((box,i)=>{   
-                        const {topRow,rightCol,bottomRow,leftCol}=box;
-                        return(<div key={i} className="bounding-box" style={{top:topRow, right:rightCol,bottom:bottomRow,left:leftCol}}></div>);
-                    })
-                    }
+                    {faces}
             </div>
        </div>
     )
