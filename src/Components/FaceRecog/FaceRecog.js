174d833fa712b247c7318f5929a5a14e24e51b3d
bhaveshkys <bhaveshsharma8@gmail.com>
2021-07-10 21:10:23 +0530

    fixing boxes.map

--- src/Components/FaceRecog/FaceRecog.js
+++ src/Components/FaceRecog/FaceRecog.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './FaceRecog.css';
 import 'tachyons';
-function FaceRecog( { imageurl, boxes } ) {
+function FaceRecog=( { imageurl, boxes } )=> {
     return (
         <div className='mw6 center '>
             <div className='  absolute mt2' >
                 <img id="inputimage" className='mt2'   src={imageurl} alt='' width='500px' height='auto' />
                 {
                     boxes.map((box,i)=>{   
-                        return(<div key={i} className="bounding-box" style={{top:box.topRow, right:box.rightCol,bottom:box.bottomRow,left:box.leftCol}}></div>)
+                        return(<div key={i} className="bounding-box" style={{top:box.topRow, right:box.rightCol,bottom:box.bottomRow,left:box.leftCol}}></div>);
                     })
                 }
             </div>
        </div>
-    );
+    )
 }
 export default FaceRecog;  
